b710fe581435f0071ed6d89e3dc3a80976bdd46d
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2024-03-22 00:00:04 +0300

    pascals triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -4,10 +4,9 @@ This module defines a function def pascal_triangle(n):
 that returns a list of lists of integers representing
 the Pascal’s triangle of n:
 """
-from typing import List
 
 
-def pascal_triangle(n: int) -> List[List[int]]:
+def pascal_triangle(n):
     """
     This function def pascal_triangle(n):
     that returns a list of lists of integers representing
@@ -17,9 +16,9 @@ def pascal_triangle(n: int) -> List[List[int]]:
     if n <= 0:
         return []
 
-    triangle: List[List[int]] = [[1]]
+    triangle = [[1]]
     for i in range(1, n):
-        row: List[int] = [1]
+        row = [1]
         for j in range(1, i):
             row.append(triangle[i-1][j-1] + triangle[i-1][j])
         row.append(1)
