4a4e00b4e264190e11bd6a15f23eb5b5037efd9e
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2024-04-04 21:37:24 +0300

    utf-8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -7,18 +7,21 @@ from typing import List
 
 
 def validUtf8(data: List[int]) -> bool:
-        
-        count = 0                         
+        """checks if data set is a valid utf-8 encoding"""
+        count = 0
         for byte in data:                          
             byte|= 256 
-			
 
             if (byte >> 3 == 0b1_11111 or 
                 (byte >> 6 == 0b1_10)^(count>0)):
                 return False
-            if   byte >> 5 == 0b1_110 : count = 1 
-            elif byte >> 4 == 0b1_1110: count = 2 
-            elif byte >> 4 == 0b1_1111: count = 3
-            elif byte >> 6 == 0b1_10  : count-= 1 
+            if byte >> 6 == 0b1_10:
+                count = count - 1
+            elif byte >> 4 == 0b1_1111:
+                count = 3
+            elif byte >> 4 == 0b1_1110:
+                count = 2
+            if   byte >> 5 == 0b1_110 :
+                count = 1
 
-        return not count                   
+        return not count
