a2b512921bfb83471b93ec25cfe378c4d065ca46
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2024-04-24 18:57:55 +0300

    rotate matrix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -3,7 +3,7 @@
 """
 
 
-def rotate_2d_matrix(matrix: list[list[int]]) -> None:
+def rotate_2d_matrix(matrix):
     """Rotates a 2D matrix 90 degrees, clockwise
     """
     left, right = 0, len(matrix) - 1
