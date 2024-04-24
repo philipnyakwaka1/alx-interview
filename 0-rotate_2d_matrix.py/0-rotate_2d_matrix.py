ea64870f98e8fe32be3f9aac6adeace8f70cfa95
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2024-04-24 18:40:21 +0300

    rotate matrix 90 degrees

---
+++ 0-rotate_2d_matrix.py/0-rotate_2d_matrix.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Module for rotate_2d_matrix function
+"""
+from typing import List
+
+
+def rotate_2d_matrix(matrix: List[List[int]]) -> None:
+    """Rotates a 2D matrix 90 degrees, clockwise
+    """
+    left, right = 0, len(matrix) - 1
+    while left < right:
+        top, bottom = left, right
+        for i in range(right):
+            tmp = matrix[top][left + i]
+            matrix[top][left + i] = matrix[bottom - i][left]
+            matrix[bottom - i][left] = matrix[bottom][right - i]
+            matrix[bottom][right - i] = matrix[top + i][right]
+            matrix[top + i][right] = tmp
+        left += 1
+        right -= 1
