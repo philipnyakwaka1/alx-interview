5686c975e97764865c84c0e36a5dfe16f7232592
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2024-05-07 19:14:45 +0300

    pycodestyle

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+0-island_perimeter module
+"""
+
+
+def island_perimeter(grid):
+    """
+    Returns the perimeter of the island described in grid
+
+    Args:
+        grid (List[List[int]]): a list of list of integers
+
+    Returns:
+        int: perimeter of the island
+    """
+    perimeter = 0
+
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == 1:
+                if i == 0 or grid[i - 1][j] == 0:
+                    perimeter += 1
+                if i == len(grid) - 1 or grid[i + 1][j] == 0:
+                    perimeter += 1
+                if j == 0 or grid[i][j - 1] == 0:
+                    perimeter += 1
+                if j == len(grid[0]) - 1 or grid[i][j + 1] == 0:
+                    perimeter += 1
+
+    return perimeter
